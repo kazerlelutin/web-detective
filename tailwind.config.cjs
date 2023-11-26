1ae8570a8bda5168c2417924444f98fa4f6444b2
Kazerlelutin <b@bouteiller.contact>
2023-11-26 15:28:50 +0100

    add template

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/partials/*.html', './src/templates/*.html'],
+  content: ['./index.html', './src/partials/*.html','./src/pages/*.html', './src/templates/*.html'],
   theme: {
     extend: {},
   },
